11f58a053ca4dde5bc032869f55dfbb2c6038927
Alexios Lampouridis <alexlampouridis@gmail.com>
2021-07-05 17:34:30 +0300

    -Minor fix at entrypoint sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,4 +11,4 @@ python manage.py migrate
 
 # Start server
 echo "Starting server"
-gunicorn -b 0.0.0.0:8000 --workers=2 --threads=4 --worker-class=gthread django_docker.wsgi
+gunicorn -b 0.0.0.0:8000 --workers=2 --threads=4 --worker-class=gthread backend.wsgi
